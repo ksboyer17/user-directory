ef5a21e3972d79b05210f0a4b337c1cc03959027
Kenny Boyer <ksboyerii@gmail.com>
2021-06-15 18:00:06 -0500

    trying to get non componenet model to work

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from "react";
 import axios from "axios";
+import Table from 'react-bootstrap/Table';
+import Table from './components/Table'
 
 function App() {
 //setting the initial array as empty
@@ -16,8 +18,46 @@ useEffect(() => {
 },[]) 
 console.log(employees);
   return (
-<div></div>
-  );
+<div>
+<Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
+   );
+ }
+)
 }
+</thead>
+</div>
+)
+ 
+
+
+ 
 
 export default App;
